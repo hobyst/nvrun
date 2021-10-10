2a88a40134d0c10f1258be7328c445344f5c2d4e
hobyst <48522412+hobyst@users.noreply.github.com>
2021-10-10 21:18:37 +0200

    Add NVIDIA Prime profile check

--- nvrun.sh
+++ nvrun.sh
@@ -18,6 +18,24 @@ if [[ ("$command" == "") || ("$command" == "--help") || ("$command" == "-h") ]];
     echo "        Show this help text."
     echo
 else    # Run command
+    # Retrieve current NVIDIA Prime profile
+    current_prime_profile=$(prime-select query)
+
+    # Check current prime-select profile
+    if [[ "$current_prime_profile" != "on-demand" ]]; then  # Check if a wrong PRIME profile is being used and notify if so
+        echo
+        echo "Current NVIDIA Prime profile is $current_prime_profile."
+        
+        # Turn the underscored profile name into something more fancy
+        case $current_prime_profile in
+            intel)  current_prime_profile_friendly_name="Intel integrated";;
+            nvidia) current_prime_profile_friendly_name="NVIDIA dedicated";;
+        esac
+
+        echo "NVIDIA GPU offloading settings will be ignored by the driver and the $current_prime_profile_friendly_name GPU will be used."
+        echo
+    fi
+
     # Notify offload to the NVIDIA GPU to the script shell environment
     export __NV_PRIME_RENDER_OFFLOAD=1
     export __VK_LAYER_NV_optimus=NVIDIA_only
